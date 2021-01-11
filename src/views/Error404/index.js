ef1b8d22a8a0b35e348b02dc52176ce8eff343d4
Bimata Prathama <bimata.prathama@gmail.com>
2021-01-11 14:35:59 +0700

    underlined links, bolder 404 page statement

--- src/views/Error404/index.js
+++ src/views/Error404/index.js
@@ -1,7 +1,21 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
 export default function Error404() {
   return (
     <main>
-      <section className="section">404 Not Found</section>
+      <section class="hero is-medium is-danger is-bold">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              404
+            </h1>
+            <h2 class="subtitle">
+              This is not the page you are looking for. <Link to="/">Move along</Link>.
+            </h2>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
